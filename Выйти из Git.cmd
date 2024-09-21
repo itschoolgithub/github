c5cb06ecfbaabf9eea19d0b140513249a6403a1a
itschoolgithub <itschoolgithub@mail.ru>
2024-09-21 21:35:23 +0500

    Update Выйти из Git.cmd

--- Выйти из Git.cmd
+++ Выйти из Git.cmd
@@ -5,3 +5,7 @@ set target=git:https://github.com
 cmdkey /delete:%target%
 
 rd /s /q "%APPDATA%\Code\User\globalStorage"
+
+git config --global user.name "itschool"
+
+git config --global user.email "itschool@tou.edu.kz"
